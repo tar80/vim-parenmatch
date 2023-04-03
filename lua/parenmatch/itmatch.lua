02892ef0f96668748c6fc285e2aff28f14f8d78d
tar80 <tar80master@gmail.com>
2023-04-03 21:35:32 +0900

    feat: dot-repeat supported
    And Supported ts.get_node() changed in version 0.10

--- lua/parenmatch/itmatch.lua
+++ lua/parenmatch/itmatch.lua
@@ -8,81 +8,38 @@
 local itmatch = {}
 local matcher = {}
 
-if type(package.loaded["nvim-treesitter"]) ~= "table" then
-  return vim.notify("[itmatch] Treesitter not loaded", 3)
+local ts = vim.treesitter
+local api = vim.api
+
+local TITLE = 'itmatch'
+
+if type(package.loaded['nvim-treesitter']) ~= 'table' then
+  return vim.notify(string.format('[%s] Treesitter not loaded', TITLE), 3, { title = TITLE })
 end
 
----@package
----define which meta-table in tsnode to use
-local function expand_tskey()
-  local ft = vim.bo.filetype
-  local contents = matcher[ft]
-  local results = {}
-
-  if not contents then
-    return false
-  end
-
-  for k, v in pairs(contents) do
-    if k == "s" then
-      vim.tbl_map(function(key)
-        results[key] = "end_"
-      end, v)
-    elseif k == "e" then
-      vim.tbl_map(function(key)
-        results[key] = "start"
-      end, v)
-    end
-  end
-
-  return results
-end
-
----@package
----move cursor to match keyword
-local function cursor_to_match()
-  local focus_target = expand_tskey() or {}
-  local cword = vim.fn.expand("<cword>")
-  local mt = focus_target[cword:lower()]
-
-  if not mt then
-    vim.fn.feedkeys("%", "n")
-    return
-  end
-
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  local ts = vim.treesitter
-  local node = ts.get_node_at_pos(0, row - 1, col, {})
-  local pair_row, pair_col = node[mt](node)
-
-  if mt == "start" then
-    pair_col = pair_col + 1
-  end
-
-  vim.fn.cursor(pair_row + 1, pair_col)
-
-  if mt == "start" and not vim.tbl_contains(vim.tbl_keys(focus_target), vim.fn.expand("<cword>")) then
-    vim.fn.feedkeys("w", "n")
-  end
-end
-
----@package
 ---matching keywords for each filetype
 local function set_default()
   matcher = {
-    lua = { s = { "function", "if", "while", "repeat", "for", "do" }, e = { "end", "until" } },
+    lua = { s = { 'function', 'if', 'while', 'repeat', 'for', 'do' }, e = { 'end', 'until' } },
     vim = {
-      s = { "function", "fu", "if", "while", "wh", "for", "try" },
-      e = { "endfunction", "endf", "endif", "en", "endwhile", "endw", "endfor", "endfo", "endtry", "endt" },
+      s = { 'function', 'fu', 'if', 'while', 'wh', 'for', 'try' },
+      e = { 'endfunction', 'endf', 'endif', 'en', 'endwhile', 'endw', 'endfor', 'endfo', 'endtry', 'endt' },
     },
   }
+end
+
+---@param mode string Current edit mode
+---@return boolean # Is operator-command or not
+local function is_operator(mode)
+  return mode:find('^no') and true
 end
 
 ---@param opts table user configration
 ---@param append function `config.tbl_append(base_tbl, addition_tbl)`
 itmatch.setup = function(opts, append)
   if not opts then
-    return set_default()
+    set_default()
+    return
   end
 
   for key, value in pairs(opts) do
@@ -98,13 +55,88 @@ itmatch.setup = function(opts, append)
       end
     end
   end
-  print(vim.inspect(matcher))
+end
+
+---define which meta-table in tsnode to use
+---@return table # Match definition of filetype
+local function expand_tskey()
+  local ft = vim.bo.filetype
+  local contents = matcher[ft]
+  local t = {}
+
+  if contents then
+    for k, v in pairs(contents) do
+      if k == 's' then
+        vim.tbl_map(function(key)
+          t[key] = 'end_'
+        end, v)
+      elseif k == 'e' then
+        vim.tbl_map(function(key)
+          t[key] = 'start'
+        end, v)
+      end
+    end
+  end
+
+  return t
+end
+
+local function detect_items(col, mode, words)
+  local line = api.nvim_get_current_line()
+  local keywords = vim.tbl_keys(words)
+  local digit, int
+
+  for _, v in ipairs(keywords) do
+    int = line:sub(col + 1):find('[%s%(%[%{,]' .. v .. '[%s%(%[%{]')
+    if int then
+      digit = int
+      break
+    end
+  end
+
+  local input = digit and string.format('%s%s', digit, 'l') or string.format('%s%%', is_operator(mode) and 'v' or '')
+
+  api.nvim_command(string.format('normal! %s', input))
+end
+
+local function match_node(row, col, mode, point, words)
+  local node = ts.get_node and ts.get_node({ bufnr = 0, pos = { row - 1, col } })
+    or ts.get_node_at_pos(0, row - 1, col, {})
+  local pair_row, pair_col = node[point](node)
+
+  if point == 'start' then
+    pair_col = pair_col + 1
+  end
+
+  api.nvim_command(string.format('normal! %s%sgg0%sl', is_operator(mode) and 'v' or '', pair_row + 1, pair_col - 1))
+
+  if point == 'start' and not vim.tbl_contains(vim.tbl_keys(words), vim.fn.expand('<cword>')) then
+    api.nvim_feedkeys('w', 'n', false)
+  end
+end
+
+---move cursor to match keyword
+itmatch.alignment = function()
+  local mode = vim.fn.mode(1)
+  local vcount = vim.v.count1
+  local row, col = unpack(api.nvim_win_get_cursor(0))
+  local words = expand_tskey()
+  local cword = vim.fn.expand('<cword>'):lower()
+  local point = words[cword]
+
+  if vcount > 1 then
+    api.nvim_command(string.format('normal! %s%%', vcount))
+  elseif not point then
+    detect_items(col, mode, words)
+  else
+    match_node(row, col, mode, point, words)
+  end
 end
 
 set_default()
 
-vim.keymap.set({"n", "v"}, "%", function()
-  cursor_to_match()
-end, { silent = true })
+vim.keymap.set({ 'n', 'x', 'o' }, '%', function()
+  return '<Cmd>lua require("parenmatch.itmatch").alignment()<CR>'
+end, { expr = true, desc = 'itmatch' })
 
 return itmatch
